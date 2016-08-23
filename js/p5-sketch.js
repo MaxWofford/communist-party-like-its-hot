82a8b82e7415057932471440212ff9d711e25532
Max Wofford <max@maxwofford.com>
2016-08-23 07:41:38 -0400

    Various changes (see desc.)
    Add middle and footer sections
    Add sun.svg
    Move assets to assets/

--- js/p5-sketch.js
+++ js/p5-sketch.js
@@ -1,10 +1,28 @@
 var starSpeed, maxDepth, stars, starCount, maxIterations;
 
 function preload() {
-  hackClubOutline = loadImage('img/hack-club-outline.png');
+  hackClubOutline = loadImage('assets/hack-club-outline.png');
+
+  // starscapeMusic = loadSound('assets/starscapeMusic.mp3'));
+  // beachMusic = loadSound('assets/beachMusic.mp3'));
 }
 
+var transitionSound = function(oldSong, newSong) {
+  for (var i = 0; i < 11; i++) {
+    let x = i;
+    setTimeout(function() {
+      oldSong.setVolume(1 - x / 10);
+      newSong.setVolume(x / 10);
+    }, x * 100);
+  }
+};
+
 function setup() {
+  // starscapeMusic.loop();
+  // starscapeMusic.setVolume(1);
+  // beachMusic.loop();
+  // beachMusic.setVolume(0);
+
   maxDepth = 2000;
   starCount = 1000;
   maxRadius = 12;
@@ -40,7 +58,7 @@ function draw() {
   // Logo iterations should be the maxIterations when the mouse is at the center of the screen, and decrease when the mouse gets further away;
   var logoIterations = Math.max(Math.ceil((height - 2 * Math.abs((height / 2) - mouseY)) / (height / maxIterations)), 1);
   for (var x = 0; x < logoIterations; x++) {
-    var scale = (0.5 + 0.1 * x);
+    var scale = (0.5 + 0.05 * x * (x * 0.5));
     var imgWidth = hackClubOutline.width * scale;
     var imgHeight = hackClubOutline.height * scale;
     image(hackClubOutline, // p5 image object
